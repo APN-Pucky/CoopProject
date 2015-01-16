42e1ced80570a6f6c66727781093fb44eec936b4
dfb159 <dfb159@users.noreply.github.com>
2015-01-16 16:23:33 +0100

    First nice packet

---
+++ CoopProject/src/gravity/MoveableMass.java
@@ -0,0 +1,35 @@
+package gravity;
+
+import GLOOP.GLKugel;
+import GLOOP.GLTextur;
+import GLOOP.GLVektor;
+
+public class MoveableMass extends GLKugel {
+
+	public MoveableMass(GLVektor pPosition, double pRadius) {
+		super(pPosition, pRadius);
+	}
+
+	public MoveableMass(GLVektor pPosition, double pRadius, GLTextur pTextur) {
+		super(pPosition, pRadius, pTextur);
+	}
+
+	public MoveableMass(GLVektor pPosition, double pRadius, String pTextur) {
+		super(pPosition, pRadius, pTextur);
+	}
+
+	public MoveableMass(double pX, double pY, double pZ, double pRadius) {
+		super(pX, pY, pZ, pRadius);
+	}
+
+	public MoveableMass(double pX, double pY, double pZ, double pRadius,
+			GLTextur pT) {
+		super(pX, pY, pZ, pRadius, pT);
+	}
+
+	public MoveableMass(double pX, double pY, double pZ, double pRadius,
+			String pTextur) {
+		super(pX, pY, pZ, pRadius, pTextur);
+	}
+
+}
