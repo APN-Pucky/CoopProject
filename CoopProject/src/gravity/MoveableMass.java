3b6432376ae2c8a6f5a509fd879601f6144d40eb
dfb159 <dfb159@users.noreply.github.com>
2015-01-16 16:28:45 +0100

    tuj

--- CoopProject/src/gravity/MoveableMass.java
+++ CoopProject/src/gravity/MoveableMass.java
@@ -8,28 +8,37 @@ public class MoveableMass extends GLKugel {
 	GLVektor speed, force;
 	public MoveableMass(GLVektor pPosition, double pRadius) {
 		super(pPosition, pRadius);
+		initial();
 	}
 
 	public MoveableMass(GLVektor pPosition, double pRadius, GLTextur pTextur) {
 		super(pPosition, pRadius, pTextur);
+		initial();
 	}
 
 	public MoveableMass(GLVektor pPosition, double pRadius, String pTextur) {
 		super(pPosition, pRadius, pTextur);
+		initial();
 	}
 
 	public MoveableMass(double pX, double pY, double pZ, double pRadius) {
 		super(pX, pY, pZ, pRadius);
+		initial();
 	}
 
 	public MoveableMass(double pX, double pY, double pZ, double pRadius,
 			GLTextur pT) {
 		super(pX, pY, pZ, pRadius, pT);
+		initial();
 	}
 
 	public MoveableMass(double pX, double pY, double pZ, double pRadius,
 			String pTextur) {
 		super(pX, pY, pZ, pRadius, pTextur);
+		initial();
 	}
-
+	public void initial() {
+		speed = new GLVektor(0,0,0);
+		force = new GLVektor(0,0,0);
+	}
 }
