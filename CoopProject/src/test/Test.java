aad2d0d2fa9b064e9c25917dd89706a4e1281467
APN-Pucky <APN-Pucky@users.noreply.github.com>
2015-01-16 19:50:17 +0100

    Changed Kamera

--- CoopProject/src/test/Test.java
+++ CoopProject/src/test/Test.java
@@ -1,12 +1,15 @@
 package test;
 
-import GLOOP.*;
+import GLOOP.GLKugel;
+import GLOOP.GLLicht;
+import GLOOP.GLMaterial;
+import GLOOP.GLSchwenkkamera;
 
 public class Test 
 {
 	public static void main(String[] args)
 	{
-		new GLEntwicklerkamera();
+		new GLSchwenkkamera();
 		new GLLicht();
 		
 		new GLKugel(0,0,0, 10).setzeMaterial(GLMaterial.GUMMI);
