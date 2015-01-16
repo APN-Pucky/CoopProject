7c5450bbc1b4a08c34ec87dfac4da1879b150964
dfb159 <dfb159@users.noreply.github.com>
2015-01-16 23:55:12 +0100

    End of the day
    some changes to the material

--- CoopProject/src/gravity/Material.java
+++ CoopProject/src/gravity/Material.java
@@ -5,4 +5,12 @@ public class Material {
 	final static float Au = 196.97F;
 	final static float H = 1.008F;
 	final static float Uuq = 289F;
+	
+	double dichte;
+	public double getDichte() {
+		return dichte;
+	}
+	public void setDichte(double dichte) {
+		this.dichte = dichte;
+	}
 }
