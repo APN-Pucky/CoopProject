16a6fda7605acf19d917c72cf2822ebdb7c25ad6
dfb159 <dfb159@users.noreply.github.com>
2015-01-17 12:05:10 +0100

    Synchronized

--- CoopProject/src/gravity/MoveableMass.java
+++ CoopProject/src/gravity/MoveableMass.java
@@ -24,19 +24,25 @@ public class MoveableMass extends GLKugel {
     }
 	public void setMass(double pMass) {
 		this.mass = mass;
-        //Matter=null -> NPE?
 		this.setzeSkalierung(Math.pow((3/4) * mass / matter.getDichte() / Math.PI,1/3));
 	}
-  //Matter=null -> NPE
+//  Matter=null -> NPE
 	public void setRadius(double pRadius) {
 		this.setzeSkalierung(pRadius);
 		double volume = (4/3) * Math.PI * Math.pow(pRadius, 3);
 		mass = volume * matter.getDichte();
 	}
-	public void setForce(GLVektor pForce) {
-		
+	synchronized public void setForce(GLVektor pForce) {
+		this.force = pForce;
 	}
-	public void setSpeed() {
-		
+	public void calcSpeed(double time) {
+		GLVektor pForce = new GLVektor(force);
+		pForce.multipliziere(time);
+		speed.addiere(pForce);
+	}
+	public void move(double time) {
+		GLVektor pSpeed = new GLVektor(speed);
+		pSpeed.multipliziere(time);
+		this.verschiebe(pSpeed);
 	}
 }
