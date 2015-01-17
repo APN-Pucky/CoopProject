f8f27b93531e7823714d6b1e3febdcd9e7ef4999
dfb159 <dfb159@users.noreply.github.com>
2015-01-17 23:32:46 +0100

    mit debug und 500ns pro run aufm laptop

--- CoopProject/src/gravity/RenderUpdateThread.java
+++ CoopProject/src/gravity/RenderUpdateThread.java
@@ -17,6 +17,8 @@ public class RenderUpdateThread extends Thread
 	public void run()
 	{
 		lasttime = System.nanoTime()/1_000_000_000D;
+		long firstTime = System.nanoTime();
+		int i = 0;
 		while(running)
 		{
 			delta = lasttime;
@@ -25,6 +27,10 @@ public class RenderUpdateThread extends Thread
 			for(Updateable u : Global.updates)
 			{
 				u.update(delta);//Unabhängig von der Rendergeschwindigkeit bewegen/updaten
+			}
+			if(++i>=1_000_000) {
+				System.out.println(i + " mal - " + (System.nanoTime() - firstTime )/ i + " durchschnitt");
+				i=0; firstTime = System.nanoTime();
 			}
 		}
 	}
