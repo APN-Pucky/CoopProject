4070ab0c3a303b8466b5836d2b153ebbe2135702
APN-Pucky <APN-Pucky@users.noreply.github.com>
2015-01-18 14:14:40 +0100

    Kamera

--- CoopProject/src/gravity/Global.java
+++ CoopProject/src/gravity/Global.java
@@ -2,8 +2,11 @@ package gravity;
 
 import java.util.ArrayList;
 
+import GLOOP.GLKamera;
+
 public class Global 
 {
 	public static ArrayList<Updateable> updates = new ArrayList<Updateable>();
 	public static ArrayList<MoveableMass> mma = new ArrayList<MoveableMass>();
+	public static GLKamera gk;
 }
